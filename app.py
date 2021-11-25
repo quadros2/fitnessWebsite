fe7f5fb97d35d049065069c9dee0f0483d083a0d
ckesana <ckesan2@illinois.edu>
2021-11-25 17:21:07 -0600

    Add viewing for userExercises and usersFoods tables and CRUD for the logged in user's history tables

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 from yaml import load, Loader
 import os
-import pymysql
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'KEY'
+globaluserid = 0
 
 #lloyd
 def init_connect_engine():
@@ -34,10 +33,6 @@ conn = engine.connect()
 #chan
 @app.route('/food', methods=['POST', 'GET'])
 def food():
-    #grabbing parameters from login page
-    username = request.args.get('username')
-    password = request.args.get('password')
-
     all_data = conn.execute("SELECT * FROM Food;").fetchall()
     return render_template("food.html", foods=all_data)
 
@@ -111,16 +106,20 @@ def totalBurned():
 
 @app.route('/login', methods=["POST"])
 def login():
+    global globaluserid
     username = request.form["username"]
     password = request.form["password"]
 
     # check if username and password combo exists SELECT count(*) FROM Users WHERE username = (%s) AND  password = (%s)
     sql1 = "SELECT * FROM Users WHERE username = \"{}\" AND password = \"{}\"".format(username, password)
-    if len(conn.execute(sql1).fetchall()) == 0:
-        flash("NOPE", category='success')
+
+    row = conn.execute(sql1).fetchone()
+    if len(row) == 0:
+        flash("NOPE", category='error')
         return redirect(url_for('chandrachur'))
     else:
-        #pass username and password params to the food URL
+        # pass userId to the food URL
+        globaluserid = row[0]
         return redirect(url_for('renderMain'))
     
 @app.route('/signup', methods=["POST"])
@@ -134,7 +133,7 @@ def signup():
     # check if username exists
     sql1 = "SELECT * FROM Users WHERE username = \"{}\"".format(username)
     if len(conn.execute(sql1).fetchall()) == 1:
-        flash("NOPE")
+        flash("NOPE", category='error')
         return redirect(url_for('rendersignup'))
     
     #create a unique userid using newUserId = max(userId) + 1
@@ -155,14 +154,141 @@ def signup():
     return redirect(url_for('/renderMain'))
 
 
+@app.route('/users_foods', methods=['POST', 'GET'])
+def users_foods():
+
+    sql = "SELECT * FROM UsersFoods WHERE userId = (%s)"
+    params = (globaluserid)
+    all_data = conn.execute(sql, params).fetchall()
+
+    return render_template("userFoods.html", users_foods=all_data)
+
+
+@app.route('/users_exercises', methods=['POST', 'GET'])
+def users_exercises():
+
+    sql = "SELECT * FROM UsersExercises WHERE userId = (%s)"
+    params = (globaluserid)
+    all_data = conn.execute(sql, params).fetchall()
+
+    return render_template("userExercises.html", users_exercises=all_data)
+
+
+@app.route('/foodHistory', methods=['POST', 'GET'])
+def foodHistory():
+
+    sql = "SELECT * FROM UsersFoods NATURAL JOIN FoodHistory WHERE userId = (%s)"
+    params = (globaluserid)
+    all_data = conn.execute(sql, params).fetchall()
+
+    return render_template("foodHistory.html", foodhists=all_data)
+
+
+@app.route('/foodHistory_insert', methods=['POST'])
+def foodHistory_insert():
+    if request.method == 'POST':
+        usersFoodsId = request.form['usersFoodsId']
+        date = request.form['date']
+        quantity = request.form['quantity']
+
+        sql = "INSERT INTO FoodHistory (usersFoodsId, date, quantity) VALUES (%s, %s, %s)"
+        params = (usersFoodsId, date, quantity)
+        conn.execute(sql, params)
+
+        flash("Food History Inserted Successfully", category='success')
+        return redirect(url_for('foodHistory'))
+
+
+@app.route('/foodHistory_update', methods=['GET', 'POST'])
+def foodHistory_update():
+    if request.method == 'POST':
+
+        foodHistoryId = request.form.get('foodHistoryId')
+        newusersFoodsId = request.form['usersFoodsId']
+        newdate = request.form['date']
+        newquantity = request.form['quantity']
+
+        sql = "UPDATE FoodHistory SET usersFoodsId = (%s), date = (%s), quantity = (%s) WHERE foodHistoryId = (%s)"
+        params = (newusersFoodsId, newdate, newquantity, foodHistoryId)
+        conn.execute(sql, params)
+
+        flash("Food History Updated Successfully", category='success')
+        return redirect(url_for('foodHistory'))
+
+
+@app.route('/foodHistory_delete/<foodHistoryId>/', methods=['GET', 'POST'])
+def foodHistory_delete(foodHistoryId):
+    sql = "DELETE FROM FoodHistory WHERE foodHistoryId = (%s)"
+    params = (foodHistoryId)
+    conn.execute(sql, params)
+
+    flash("Food History Deleted Successfully", category='success')
+    return redirect(url_for('foodHistory'))
+
+
+@app.route('/exerciseHistory', methods=['POST', 'GET'])
+def exerciseHistory():
+
+    sql = "SELECT * FROM UsersExercises NATURAL JOIN ExerciseHistory WHERE userId = (%s)"
+    params = (globaluserid)
+    all_data = conn.execute(sql, params).fetchall()
+
+    return render_template("exerciseHistory.html", exhists=all_data)
+
+
+@app.route('/exerciseHistory_insert', methods=['POST'])
+def exerciseHistory_insert():
+    if request.method == 'POST':
+        usersExercisesId = request.form['usersExercisesId']
+        date = request.form['date']
+        duration = request.form['duration']
+
+        sql = "INSERT INTO ExerciseHistory (usersExercisesId, date, duration) VALUES (%s, %s, %s)"
+        params = (usersExercisesId, date, duration)
+        conn.execute(sql, params)
+
+        flash("Exercise History Inserted Successfully", category='success')
+        return redirect(url_for('exerciseHistory'))
+
+
+@app.route('/exerciseHistory_update', methods=['GET', 'POST'])
+def exerciseHistory_update():
+    if request.method == 'POST':
+
+        exerciseHistoryId = request.form.get('exerciseHistoryId')
+        newusersExercisesId = request.form['usersExercisesId']
+        newdate = request.form['date']
+        newduration = request.form['duration']
+
+        sql = "UPDATE ExerciseHistory SET usersExercisesId = (%s), date = (%s), duration = (%s) " \
+              "WHERE exerciseHistoryId = (%s)"
+        params = (newusersExercisesId, newdate, newduration, exerciseHistoryId)
+        conn.execute(sql, params)
+
+        flash("Exercise History Updated Successfully", category='success')
+        return redirect(url_for('exerciseHistory'))
+
+
+@app.route('/exerciseHistory_delete/<exerciseHistoryId>/', methods=['GET', 'POST'])
+def exerciseHistory_delete(exerciseHistoryId):
+    sql = "DELETE FROM ExerciseHistory WHERE exerciseHistoryId = (%s)"
+    params = (exerciseHistoryId)
+    conn.execute(sql, params)
+
+    flash("Exercise History Deleted Successfully", category='success')
+    return redirect(url_for('exerciseHistory'))
+
+
 @app.route('/rendersignup', methods=["POST"])
 def rendersignup():
     return render_template("signup.html")
 
+
 @app.route('/renderMain')
 def renderMain():
     return render_template("menu.html")
-    
+
+
 @app.route('/', methods=["POST", "GET"])
 def chandrachur():
     return render_template("login.html")
