eae99a3178f5cd54362642b12e75cec9c9fde6b8
ckesana <ckesan2@illinois.edu>
2021-11-26 16:05:26 -0600

    add logic for getting logged in user's total calories consumed and burned

--- app.py
+++ app.py
@@ -279,6 +279,32 @@ def exerciseHistory_delete(exerciseHistoryId):
     return redirect(url_for('exerciseHistory'))
 
 
+@app.route('/userTotalCals', methods=['POST', 'GET'])
+def userTotalCals():
+    sql = "SELECT firstName, lastName, date, SUM(fh.quantity * f.calories) as totalCals " \
+          "FROM FoodHistory fh NATURAL JOIN UsersFoods uf NATURAL JOIN Users u NATURAL JOIN Food f " \
+          "WHERE userId = (%s)" \
+          "GROUP BY userId, firstName, lastName, date " \
+          "ORDER BY firstName, lastName"
+
+    params = (globaluserid)
+    all_data = conn.execute(sql, params).fetchall()
+    return render_template("userTotalCals.html", totals=all_data)
+
+
+@app.route('/userTotalBurned', methods=['POST', 'GET'])
+def userTotalBurned():
+    sql = "SELECT firstName, lastName, date, SUM(duration * caloriesBurned) as totalBurned " \
+          "FROM ExerciseHistory eh NATURAL JOIN UsersExercises ue NATURAL JOIN Users u NATURAL JOIN Exercise e " \
+          "WHERE userId = (%s)" \
+          "GROUP BY userId, firstName, lastName, date " \
+          "ORDER BY firstName, lastName"
+
+    params = (globaluserid)
+    all_data = conn.execute(sql, params).fetchall()
+    return render_template("userTotalBurned.html", totals=all_data)
+
+
 @app.route('/rendersignup', methods=["POST"])
 def rendersignup():
     return render_template("signup.html")
